14a06154b332cafa1246d093fa5292970d41e05a
nishikireo <62166194+nishikireo@users.noreply.github.com>
2020-03-17 11:58:42 +0900

    Create confirm.php

---
+++ confirm.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="utf-8">
+    <title>太郎のポートフォリオサイト</title>
+    <link rel="icon" href="favicon.ico">
+    <meta name="description" content="太郎のポートフォリオサイトです">
+    <link rel="stylesheet" href="css/styles.css">
+  </head>
+  <body>
+    <p>こんにちは！</p>
+    <p><?php $_POST['name'] ?></p>
+    
+  </body>
+
+
+  
+</html>
